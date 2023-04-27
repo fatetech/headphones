e81d5f57de500a25bb28668c14b1506c98859ddb
iamFate <stephalex222@gmail.com>
2023-04-27 01:46:45 -0700

    update

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+        const $gallery = $('.gallery a').simpleLightbox();
+      });
+
+
+      const Fame = document.getElementById('top');
+     Fame.addEventListener('click', function(){
+      window.scrollTo(0, 0);
+     })
